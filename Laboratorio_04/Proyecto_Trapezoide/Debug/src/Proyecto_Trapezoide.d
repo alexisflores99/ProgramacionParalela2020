48ec2dc8a1939c1fc09133d1120c60d9c593dd95
alexisflores99 <alexis.bingo.flores@gmail.com>
2020-01-25 15:04:21 -0500

    Borrando un directorio incompleto

--- Laboratorio_04/Proyecto_Trapezoide/Debug/src/Proyecto_Trapezoide.d
+++
@@ -1,7 +0,0 @@
-src/Proyecto_Trapezoide.o: ../src/Proyecto_Trapezoide.c \
- /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
- /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
-
-/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
-
-/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:
